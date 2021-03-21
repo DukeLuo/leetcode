0d00a541f8f4ee597af7345e1370e2a25d0bf5a5
duke <dukeluo@outlook.com>
2021-03-21 05:05:57 -0400

    solve 101 using iteration

--- _101SymmetricTree.java
+++ _101SymmetricTree.java
@@ -1,3 +1,6 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
 class TreeNode {
     int val;
     TreeNode left;
@@ -33,11 +36,39 @@ public class _101SymmetricTree {
         return leftTree.val == rightTree.val && isMirror(leftTree.left, rightTree.right) && isMirror(leftTree.right, rightTree.left);
     }
 
+    public boolean isSymmetricByIteration(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Deque<TreeNode> nodeStack = new LinkedList<>();
+        nodeStack.add(root);
+        nodeStack.add(root);
+        while (!nodeStack.isEmpty()) {
+            TreeNode left = nodeStack.pop();
+            TreeNode right = nodeStack.pop();
+            if (left == null && right == null) {
+                continue;
+            }
+            if (left == null || right == null) {
+                return false;
+            }
+            if (left.val != right.val) {
+                return false;
+            }
+            nodeStack.add(left.left);
+            nodeStack.add(right.right);
+            nodeStack.add(left.right);
+            nodeStack.add(right.left);
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         TreeNode leftTree = new TreeNode(2);
         TreeNode rightTree = new TreeNode(2);
         TreeNode root = new TreeNode(1, leftTree, rightTree);
         _101SymmetricTree solution = new _101SymmetricTree();
         System.out.println(solution.isSymmetric(root));
+        System.out.println(solution.isSymmetricByIteration(root));
     }
 }
