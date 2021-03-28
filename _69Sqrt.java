0f2567b6afb9e203870e19700330a27cee590550
duke <dukeluo@outlook.com>
2021-03-28 11:18:47 -0400

    solve 69

---
+++ _69Sqrt.java
@@ -0,0 +1,17 @@
+public class _69Sqrt {
+    public int mySqrt(int x) {
+        double result = x;
+        double diff = 1;
+        while (diff < Math.abs(x - result * result)) {
+            result = (result + x / result) / 2.0;
+        }
+        return (int)result;
+    }
+
+    public static void main(String[] args) {
+        _69Sqrt solution = new _69Sqrt();
+        System.out.println(solution.mySqrt(2147395599));
+        System.out.println(solution.mySqrt(4));
+        System.out.println(solution.mySqrt(0));
+    }
+}
